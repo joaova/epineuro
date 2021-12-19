3bc8b0751acfb4e3c68fd8ea1f54668e88db01b9
Joao Amaral <joaovillanova@hotmail.com.br>
2021-12-19 09:30:16 -0300

    Correção patient info [Object]

--- src/app/modules/admin/pages/patient-info/patient-info.component.ts
+++ src/app/modules/admin/pages/patient-info/patient-info.component.ts
@@ -1,7 +1,7 @@
 import { PatientCompleteDTO } from './../../../../core/model/PatientCompleteDTO';
 import { FormDataService } from './../../../../core/services/form-data.service';
 import { Component, OnInit } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-patient-info',
   templateUrl: './patient-info.component.html',
@@ -12,6 +12,12 @@ export class PatientInfoComponent implements OnInit {
   patient: PatientCompleteDTO;
   sexo = ['Feminino', 'Masculino'];
   bol = ['não', 'sim']
+  comorbities: string[] = [];
+  history: string[] = [];
+  drugs: string[] = [];
+  medications: string[] = [];
+  exams: string[] = [];
+  surgery: string[] = [];
 
   constructor(
     private patientDataService: FormDataService
@@ -21,6 +27,31 @@ export class PatientInfoComponent implements OnInit {
     this.patientDataService.currentMessagePessoa.subscribe((patient) => {
       this.patient = patient;
     })
+
+    this.patient.comorbities.forEach(d => {
+      this.comorbities.push(d.id);
+    })
+
+    this.patient.drugs.forEach(dr => {
+      this.drugs.push(dr.name);
+    })
+
+    this.patient.previousNeurosurgery.forEach(s => {
+      this.surgery.push(s.name);
+    })
+
+    this.patient.firstDegreeRelative.forEach(f => {
+      this.history.push(f.id);
+    })
+
+    this.patient.medications.forEach(m => {
+      this.medications.push(m.id);
+    })
+
+    this.patient.exams.forEach(e => {
+      this.exams.push(e.name);
+    })
+
   }
   
 }
